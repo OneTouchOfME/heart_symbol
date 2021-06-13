8db5246af3e4ba9c423a6cd4740165c9da055b31
OneTouchOfME <jack7067foru@gmail.com>
2021-06-13 21:10:08 +0530

    created heart diagram using heart equation

---
+++ heart_equation.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-1, 1, 100)
+plt.title('Heart', fontsize=24)
+
+y = np.sqrt(1-np.power(x, 2))+np.power(np.square(x), 1/3)
+y1= -np.sqrt(1-np.power(x, 2))+np.power(np.square(x), 1/3)
+plt.plot(x, y, 'b')
+plt.plot(x, y1, 'b')
+my_x_ticks = np.arange(-2, 2.5, 0.5)
+my_y_ticks = np.arange(-2, 2.5, 0.5)
+plt.xticks(my_x_ticks)
+plt.yticks(my_y_ticks)
+# plt.fill_between(x, y, y1,color="red",alpha=0.9)
+plt.grid(False)
+# plt.fill()
+plt.show()
